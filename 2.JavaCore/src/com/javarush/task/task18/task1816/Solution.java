8e4f49e04c91f6e70fa56e797c760a22fa907430
gangmaster <user.shadow@gmail.com>
2019-09-03 12:58:27 +0300

    Java practice tasks (InputStream/Alphabet/Matches)

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1816/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task18.task1816;
+
+/*
+Английские буквы
+*/
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        FileInputStream inputStream = new FileInputStream("/home/gangmaster/Documents/test_file");
+        List<Character> list = new ArrayList<>();
+        while (inputStream.available() > 0) {
+            list.add((char) inputStream.read());
+        }
+        inputStream.close();
+
+        int count = 0;
+        for (Character value : list) {
+            if (String.valueOf(value).matches(".*[A-Za-z].*")) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
